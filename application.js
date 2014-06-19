3e14f7932783dcb48bb162b7106fc40bcda9cfa5
Frederik Eichler <mail@frederik-eichler.de>
2014-06-19 09:43:13 +0200

    formatting

--- application.js
+++ application.js
@@ -2,10 +2,5 @@ angular.module('todoApp', []);
 
 angular.module('todoApp', []).config(function($sceProvider) {
 	// Completely disable SCE to support IE7.
-    $sceProvider.enabled(false);
-})
-/*
-angular.module('todoApp').controller('logger', ['$scope', 'loggingService',  
-  function($scope, loggingService) {
-	loggingService.register($scope);
-}]);*/
+	$sceProvider.enabled(false);
+});
--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,12 +1,13 @@
 'use strict';
 
-angular.module('todoApp').controller('TodoCtrl', ['$scope', 'todoService',  
+angular.module('todoApp').controller('TodoCtrl', ['$scope', 'todoService',
   function($scope, todoService) {
 
-      var TODOFOLDER = 28;
-      var TASKSFOLDER = 13;
-    
-      $scope.init = function() {
-        todoService.readAll($scope);      
-      }
-  }]);
+    var TODOFOLDER = 28;
+    var TASKSFOLDER = 13;
+
+    $scope.init = function() {
+      todoService.readAll($scope);
+    }
+  }
+]);
